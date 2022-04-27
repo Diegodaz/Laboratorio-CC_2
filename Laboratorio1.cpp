d441b3c0461a7700afcc482cf30e8f41b6a476a1
Diegodaz <83051094+Diegodaz@users.noreply.github.com>
2022-04-26 20:42:35 -0500

    Add files via upload

---
+++ Laboratorio1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	char  PrimerNombre;
+	char ApellidoPaterno;
+	char  ApellidoMaterno;
+	
+	
+	
+	cout<<"ingrse su primer nombre : ";
+	cin>>PrimerNombre;
+	cout<<"ingrese su apellido paterno : ";
+	cin>>ApellidoPaterno;
+	cout<<"ingrese su apellido materno : ";
+	cin>>ApellidoMaterno;
+	cout<<PrimerNombre<<ApellidoPaterno<<ApellidoMaterno<<".unsa.edu.pe";
+	return 0;
+	
+	
+}
